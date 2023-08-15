a2ec270e3b544fb2febe156e9222912febe66e62
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-08-15 20:33:24 +0800

    feat: Add implementation to count same barcodes

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,27 @@
 package pos.machine;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PosMachine {
-    public String printReceipt(List<String> barcodes) {
+    public static String printReceipt(List<String> barcodes) {
+        countSameBarcodes(barcodes);
         return null;
+    }
+
+    public static List<BarcodeListWithQuantity> countSameBarcodes(List<String> barcodes) {
+        List<BarcodeListWithQuantity> barcodeListWithQuantityList = new ArrayList<>();
+
+        List<String> uniqueBarcodes = new ArrayList<>();
+
+        barcodes.stream()
+                .filter(element -> !uniqueBarcodes.contains(element))
+                .forEach(element -> uniqueBarcodes.add(element));
+
+        uniqueBarcodes.stream()
+                .forEach(element -> barcodeListWithQuantityList.add(new BarcodeListWithQuantity(element, Collections.frequency(barcodes, element))));
+
+        return barcodeListWithQuantityList;
     }
 }
